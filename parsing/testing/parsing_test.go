675064606ae47798a31dda83db60b33115ed702e
Karl Moad <karl.moad@gmail.com>
2024-11-30 15:05:03 -0500

    parsing testing

--- parsing/testing/parsing_test.go
+++ parsing/testing/parsing_test.go
@@ -1,1 +1,23 @@
 package testing
+
+import (
+	"fmt"
+	"github.com/karlmoad/go_util_lib/parsing/dialect/grammar"
+	"github.com/karlmoad/go_util_lib/parsing/parser"
+	"os"
+	"testing"
+)
+
+func TestParsing_parser(t *testing.T) {
+
+	bites, _ := os.ReadFile("test.ebnf")
+	dialect := grammar.NewGrammarDialect()
+
+	p := parser.NewParser(string(bites), dialect)
+	fmt.Println("Starting Parse.")
+	expressions, err := p.Parse()
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(fmt.Sprintf("Number of expressions parsed: %d", len(expressions)))
+}
