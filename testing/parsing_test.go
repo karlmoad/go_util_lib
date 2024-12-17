193831171a72fc604c395fd970974eb33cad2694
Karl Moad <karl.moad@gmail.com>
2024-12-17 09:32:43 -0500

    modified to add additional baseline grammar format, antlr G4.  Mainly a duplicate of orig grammar but with minor changes

--- testing/parsing_test.go
+++ testing/parsing_test.go
@@ -2,12 +2,13 @@ package testing
 
 import (
 	"github.com/karlmoad/go_util_lib/parsing"
+	"github.com/karlmoad/go_util_lib/parsing/dialects/antlrG4"
 	"github.com/karlmoad/go_util_lib/parsing/dialects/grammar"
 	"os"
 	"testing"
 )
 
-func TestParsing_parser(t *testing.T) {
+func TestGrammar_parser(t *testing.T) {
 	bites, _ := os.ReadFile("test.txt")
 
 	dialect := grammar.NewGrammarDialect()
@@ -36,4 +37,37 @@ func TestParsing_parser(t *testing.T) {
 		t.Errorf("Expected 4 expressions of type RULE_ELEM, got %d", count)
 	}
 
+}
+
+func TestAntlrG4_parser(t *testing.T) {
+	bites, _ := os.ReadFile("test2.txt")
+
+	//src := string(bites)
+	//t.Error(src[855:871])
+
+	dialect := antlrG4.NewGrammarDialect()
+	par, err := parsing.NewParserForDialect(dialect)
+	if err != nil {
+		t.Error(err)
+	}
+
+	expressions, err := par.Parse(string(bites))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(expressions) != 8 {
+		t.Errorf("Expected 4 expressions, got %d", len(expressions))
+	}
+
+	count := 0
+	for _, expression := range expressions {
+		if expression.Elem().Kind() == grammar.RULE_ELEM {
+			count++
+		}
+	}
+
+	if count != 8 {
+		t.Errorf("Expected 4 expressions of type RULE_ELEM, got %d", count)
+	}
 }
